0ffb4eb12c3d5c24d65a8f0d827b39a3a58b551a
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-21 18:12:27 +0800

    add CourseViewSet

--- djangoapi/courses/views.py
+++ djangoapi/courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from courses.models import Course
+from courses.serializer import CourseSerializer
 
-# Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
